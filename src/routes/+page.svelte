86f83be82af0192347379017429076d325c40557
WilliamMcRoberts <wamcroberts2@gmail.com>
2023-03-03 09:34:26 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { PageData, ActionData } from './$types';
+
+	import { enhance } from '$app/forms';
+
+	export let form: ActionData;
+
+	let submitCount = 0;
+</script>
+
+<h1>Home</h1>
+
+<form
+	method="POST"
+	use:enhance={() => {
+		submitCount++;
+		return ({ update }) => {
+			// prevent resetting the form after submission
+			update({ reset: false });
+		};
+	}}
+>
+	<label for="text">Submission</label>
+	<input id="text" type="text" name="text" />
+	<button>Go</button>
+</form>
+
+<p>Submitted {submitCount} times</p>
+
+{#if form?.error}
+	{form.error}
+{:else if form?.result}
+	<p>Transformed: {form.result}</p>
+{/if}
